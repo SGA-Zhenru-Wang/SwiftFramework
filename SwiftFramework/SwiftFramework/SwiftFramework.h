dc43a68e358bc376863ac8d7569cdbebfbac1720
wang zhenru <wzz0617@126.com>
2016-12-18 19:28:59 +0900

    add workspace & swift framework project

---
+++ SwiftFramework/SwiftFramework/SwiftFramework.h
@@ -0,0 +1,19 @@
+//
+//  SwiftFramework.h
+//  SwiftFramework
+//
+//  Created by wzr on 2016/12/18.
+//  Copyright © 2016年 wzr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftFramework.
+FOUNDATION_EXPORT double SwiftFrameworkVersionNumber;
+
+//! Project version string for SwiftFramework.
+FOUNDATION_EXPORT const unsigned char SwiftFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFramework/PublicHeader.h>
+
+
