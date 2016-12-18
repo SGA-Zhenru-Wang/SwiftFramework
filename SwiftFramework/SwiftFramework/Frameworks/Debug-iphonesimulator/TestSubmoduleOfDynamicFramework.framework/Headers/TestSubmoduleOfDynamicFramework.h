b394ce75bfdd0195168732b1a7d5f4126c2488ef
wang zhenru <wzz0617@126.com>
2016-12-19 00:23:33 +0900

    Add modules & its submodule into swift framework project

---
+++ SwiftFramework/SwiftFramework/Frameworks/Debug-iphonesimulator/TestSubmoduleOfDynamicFramework.framework/Headers/TestSubmoduleOfDynamicFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestSubmoduleOfDynamicFramework.h
+//  TestSubmoduleOfDynamicFramework
+//
+//  Created by wzr on 2016/12/18.
+//  Copyright © 2016年 wzr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestSubmoduleOfDynamicFramework.
+FOUNDATION_EXPORT double TestSubmoduleOfDynamicFrameworkVersionNumber;
+
+//! Project version string for TestSubmoduleOfDynamicFramework.
+FOUNDATION_EXPORT const unsigned char TestSubmoduleOfDynamicFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestSubmoduleOfDynamicFramework/PublicHeader.h>
+
+#import <TestSubmoduleOfDynamicFramework/TestObjectOfSubmoduleDynamicFrame.h>
